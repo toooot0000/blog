5bf8741856798d5e0ef89787484dac067a824f97
Jerry <runan.ye@outlook.com>
2021-01-14 20:03:53 +0800

    add vue proj

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
-modules.export = {
-    
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '././' : '/',
+  outputDir:'dist',
+  chainWebpack: (config) => {
+  const resolve = require('path');
+    config.resolve.alias.set('img', resolve('public/img'))
+  }
 }
