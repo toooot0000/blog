5ddc8a5b6d614872aabe265ee8745d8cd0753e1b
Jerry <runan.ye@outlook.com>
2021-01-16 21:50:32 +0800

    Prettier~

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="wrapper">
-        <div class="img-ctn">
-            <img src="~@img/header/icon-big.png" alt="Icon" srcset="">
-        </div>
-        <div class="title">标题</div>
-        <div class="desc">描述</div>
-        <div class="link-list">
-            <div class="link"  v-for="(link, ind) in linkList"  :key="ind">
-                <a :href="link.href">
-                <img :src="link.picPath||'~@img/header/linkPic-'+ind+'.pgn'" :alt="'linkPic'+ind" srcset="">
-                </a>
-            </div>
-        </div>
-
+  <div class="wrapper">
+    <div class="img-ctn">
+      <img src="~@img/header/icon-big.png" alt="Icon" srcset="" />
     </div>
+    <div class="title">标题</div>
+    <div class="desc">描述</div>
+    <div class="link-list">
+      <div class="link" v-for="(link, ind) in linkList" :key="ind">
+        <a :href="link.href">
+          <img
+            :src="link.picPath || '~@img/header/linkPic-' + ind + '.pgn'"
+            :alt="'linkPic' + ind"
+            srcset=""
+          />
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -21,24 +24,25 @@ export default {
   props: {
     title: {
       type: String,
-      default: '我是标题'
+      default: "我是标题"
     },
     linkList: {
       type: Array,
-      default: ()=>{ [
-        {
-          href: '/',
-          picPath: ''
-        }
-      ]
+      default: () => {
+        [
+          {
+            href: "/",
+            picPath: ""
+          }
+        ];
       }
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.wrapper{
-    margin: 20px;
+.wrapper {
+  margin: 20px;
 }
 </style>
