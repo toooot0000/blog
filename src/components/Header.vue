19e0c97d425b1c9545cc5fbe3b39ad92b27f183a
Jerry Ye's Macbook Pro <runan.ye@outlook.com>
2021-02-08 21:00:03 +0800

    调整结构

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="wrp">
-    <div class="ctn container" :class="{
-      finished: isAnimFinished,
-    }"
+    <div
+      class="ctn container"
+      :class="{
+        finished: isAnimFinished,
+      }"
       @click="backTop"
     >
       <div class="title">
@@ -41,12 +43,12 @@ export default {
   props: {
     title: {
       type: String,
-      default: "我是标题"
+      default: "我是标题",
     },
     desc: {
       type: String,
       default:
-        "我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述"
+        "我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述",
     },
     linkList: {
       type: Array,
@@ -54,19 +56,19 @@ export default {
         return [
           {
             href: "#",
-            picPath: ""
+            picPath: "",
           },
           {
             href: "#",
-            picPath: ""
+            picPath: "",
           },
 
           {
             href: "#",
-            picPath: ""
-          }
+            picPath: "",
+          },
         ];
-      }
+      },
     },
   },
   data() {
@@ -75,83 +77,88 @@ export default {
       animStart: false,
       animEnd: false,
       isAnimFinished: false,
-    }
+    };
   },
   mounted() {
     const that = this;
 
     // * 绑定滚动动画
-    let trigger = {
-      trigger: ".wrp",
-      start: 0,
-      end: 50,
-      scrub: 0,
-      pin: false,
-      markers: true,
-      onEnter(){
-        that.isAnimFinished = false;
-      },
-      onLeave(){
-        that.isAnimFinished = true;
-      },
-      onEnterBack(){
-        console.log('Enter back');
-        that.isAnimFinished = true;
-      },
-      onLeaveBack(){
-        console.log('Leave back');
-        that.isAnimFinished = false;
-      }
-    };
-    gsap.to(".ctn", {
-      scrollTrigger: trigger,
-      scale: 0.1,
-      height: 100,
-      width: 100,
-      duration: 1,
-      background: 'white',
-      opacity: .8,
-      top: '5vh',
-      borderRadius: "999rem",
-      ease: 'none',
-    });
-    gsap.to([".seperator", ".desc", ".title", ".link-list"], {
-      scrollTrigger: trigger,
-      duration: 1,
-      opacity: 0,
-      fontSize: '0.2rem',
-    });
+    {
+      let trigger = {
+        trigger: ".wrp",
+        start: 0,
+        end: 50,
+        scrub: 0,
+        pin: false,
+        markers: true,
+        onEnter() {
+          that.isAnimFinished = false;
+        },
+        onLeave() {
+          that.isAnimFinished = true;
+        },
+        onEnterBack() {
+          console.log("Enter back");
+          that.isAnimFinished = true;
+        },
+        onLeaveBack() {
+          console.log("Leave back");
+          that.isAnimFinished = false;
+        },
+      };
+      gsap.to(".ctn", {
+        scrollTrigger: trigger,
+        scale: 0.1,
+        height: 100,
+        width: 100,
+        duration: 1,
+        background: "white",
+        opacity: 0.8,
+        top: "5vh",
+        borderRadius: "999rem",
+        ease: "none",
+      });
+      gsap.to([".seperator", ".desc", ".title", ".link-list"], {
+        scrollTrigger: trigger,
+        duration: 1,
+        opacity: 0,
+        fontSize: "0.2rem",
+      });
+    }
 
     // * 监听滚动事件
-    window.addEventListener('scroll', that.scrollEvent)
+    window.addEventListener("scroll", that.scrollEvent);
   },
-  destroyed(){
+  destroyed() {
     const that = this;
-    window.removeEventListener('scroll', that.scrollEvent)
+    window.removeEventListener("scroll", that.scrollEvent);
   },
-  methods:{
-    backTop(){
+  methods: {
+    backTop() {
       // * 返回顶部
-      const that = this
-      let timer = setInterval(() => {
-        let ispeed = Math.floor(-that.scrollTop / 5)
-        document.documentElement.scrollTop = document.body.scrollTop = that.scrollTop + ispeed
-        if (that.scrollTop === 0) {
-          clearInterval(timer)
-        }
-      }, 16)
-    },
-    scrollEvent(){
       const that = this;
-      that.scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-    }
-  }
+      let timer = setInterval(() => {
+        let ispeed = Math.floor(-that.scrollTop / 5);
+        document.documentElement.scrollTop = document.body.scrollTop =
+          that.scrollTop + ispeed;
+        if (that.scrollTop === 0) {
+          clearInterval(timer);
+        }
+      }, 16);
+    },
+    scrollEvent() {
+      const that = this;
+      that.scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @use "@assets/sass/base";
-
 
 .wrp {
   color: map-get($map: base.$colors, $key: "main");
@@ -160,7 +167,7 @@ export default {
   z-index: 1000;
 }
 
-.ctn{
+.ctn {
   background: none;
   box-shadow: none;
   text-align: center;
@@ -170,20 +177,19 @@ export default {
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
   z-index: inherit;
-  &:hover{
+  &:hover {
     box-shadow: none;
   }
-  &.finished{
+  &.finished {
     cursor: pointer;
   }
 }
 
-
-.seperator{
+.seperator {
   margin: 25px 0;
 }
 
-.desc{
+.desc {
   @include base.margin-bottom(2rem);
   opacity: inherit;
 }
@@ -193,7 +199,7 @@ export default {
   opacity: inherit;
 }
 
-.link-list{
+.link-list {
   display: flex;
   flex-flow: row nowrap;
   justify-content: center;
@@ -202,7 +208,7 @@ export default {
 
 .link-icon {
   width: 50px;
-  @include base.margin-left(.5rem);
-  @include base.margin-right(.5rem);
+  @include base.margin-left(0.5rem);
+  @include base.margin-right(0.5rem);
 }
 </style>
