238215a824e2d2b057a26262b58ba334563418bb
Jerry Ye's Macbook Pro <runan.ye@outlook.com>
2021-02-09 00:00:53 +0800

    大概框架

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        
+        <canvas id='nav-bg'></canvas>
+        <div class="cnt">
+            <ul class="tab-ctn">
+                <li class="tab">
+
+                </li>
+                <li class="tab">
+                    
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
