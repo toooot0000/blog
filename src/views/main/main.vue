a6ee0176349157bc0b827285ec1a2c5bb7549933
Jerry Ye's Macbook Pro <runan.ye@outlook.com>
2021-02-10 21:07:22 +0800

    清楚注释

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <!-- <NavBar :isActive='isHeaderAnimFinished'></NavBar> -->
     <Header v-model="isHeaderAnimFinished"></Header>
     <div class="container content">
       <div class="block"></div>
@@ -10,13 +9,10 @@
 
 <script>
 import Header from "@comps/Header.vue";
-// import NavBar from "@comps/NavBar.vue";
-
 export default {
   name: "PageMain",
   components: {
     Header,
-    // NavBar
   },
   data:()=>({
     isHeaderAnimFinished: false,
@@ -25,7 +21,6 @@ export default {
 </script>
 
 <style lang="scss">
-// @use "@assets/sass/base.scss";
 .block {
   height: 2000px;
 }
