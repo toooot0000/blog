767793780e7ffd77b62b0dd80a205d27376b60b1
Jerry Ye's Macbook Pro <runan.ye@outlook.com>
2021-02-14 23:40:46 +0800

    implete the so-called backend part

--- blogs/manage.py
+++ blogs/manage.py
@@ -3,30 +3,31 @@ import json
 import re
 import markdown2
 from pathlib import Path
-
-# basePath = os.path.dirname(os.path.realpath(__file__))  # 获取当前路径
-# configPath = os.path.join(basePath, "config.json")
-# projPath = os.path.join(basePath, '../')
+from shutil import copy
+from time import gmtime, strftime
 
 basePath = Path(__file__).parent.resolve()
 configPath = basePath/'config.json'
 projPath = (basePath/'..').resolve()
 print('Project Path         : '+str(projPath))
 
-f = open(configPath, '+')
-config = json.loads(f.read())
+configFile = open(configPath, 'r+')
+config = json.loads(configFile.read())
+configFile.close()
 
 # build path
 buildFolderPath = (projPath / config['buildPath'])
-print('Build Folder Path    : '+ str(buildFolderPath))
+print('Build Folder Path    : ' + str(buildFolderPath))
 
 # find the template file
 templatePath = (projPath / config['templatePath']).resolve()
-print('Template Path        : '+ str(templatePath))
+print('Template Path        : ' + str(templatePath))
 
 # the template reg
 subPattern = r"\[\[content\]\]"
 
+# pic formats
+picFormats = ['.ico', '.png', '.jpg', '.gif']
 
 """ 
 --add blogName           : add a blog page named blogName, show its id
@@ -42,13 +43,62 @@ def entry(app):
     # print(app.params)
 
     def add(name):
-        print('This is func. add! Copy!')
+        print('Add blog page')
+        if((basePath/name).exists()):
+            print(
+                'You already have a blog of the same name! \nPlease consider changing the name!')
+            return
+        title   = input('Blog title: ')
+        content = input("Blog desc: ")
+        time    = strftime("%Y-%m-%d", gmtime())
+        tags    = []
+        headPic = "/"
+        # create the folder name/
+        (basePath/name).mkdir()
+
+        # add the main .md file
+        with (basePath/name/(name+".md")).open('w') as mdFile:
+            mdFile.write('# ' + title + '\n')
+            pass
+
+        # add info file
+        with (basePath/name/'info.json').open('w') as infoFile:
+            info = {
+                "title": title,
+                "content": content,
+                "time": time,
+                "tags": tags,
+                "head-pic": headPic
+            }
+            infoFile.write(json.dumps(info))
+            pass
+
+        # update config
+        print('Add to blogList...')
+        config['curId'] += 1
+        _id = config['curId']
+        print('Current Id: '+str(config['curId']))
+        # print(config['blogList'])
+        config['blogList'].append(
+            {
+                'id': _id,
+                'name': name,
+                'folderPath': name,
+            }
+        )
+        # print(config['blogList'])
+        with configPath.open('w+') as configFile:
+            print('Rewrite config.json...')
+            configFile.write(json.dumps(config))
+        print("Adding blog page complete!")
         pass
 
+    # TODO
     def delete(nameOrId):
         print('This is func. delete! Copy!')
         pass
 
+    # TODO
     def init():
         print('This is func. init! Copy!')
         pass
@@ -57,14 +107,15 @@ def entry(app):
         print('Update')
         # get the template file
         try:
-            template = templatePath.open()
+            templateFile = templatePath.open()
         except:
             print('Sth wrong with template!')
             assert(False)
         # change all path to /docs/js /docs/css /docs/img
-        template = re.sub(r'\./js', '/docs/js', template.read())
+        template = re.sub(r'\./js', '/docs/js', templateFile.read())
         template = re.sub(r'\./css', '/docs/css', template)
         template = re.sub(r'\./img', '/docs/img', template)
+        templateFile.close()
 
         # loop blogList
         for blog in config['blogList']:
@@ -77,9 +128,16 @@ def entry(app):
             # print(blogFolderPath)
             blogPath = blogFolderPath/blogName
             # print(blogPath)
+
+            # TODO if blog is not modified, continue
+
             # use markdown2 to read the md file and convert it to html
-            blogHtml = markdown2.markdown_path(str(blogPath))
+            try:
+                blogHtml = markdown2.markdown_path(str(blogPath))
             # print(blogHtml)
+            except:
+                Warning('Config contains a missing blog!\nName: '+blog['name'] )
+                continue
 
             blogBuildFolderPath = buildFolderPath/blog['folderPath']
             # print(blogBuildFolderPath)
@@ -89,19 +147,23 @@ def entry(app):
 
             # substitude '[[content]]' part in the template for the blog html
             subedHtml = re.sub(subPattern, blogHtml, template)
-            # print(subedHtml)
+
             # save processed html as blog.html
             if not blogBuildFolderPath.exists():
-                # print('make dir ')
+                # make the directory
                 blogBuildFolderPath.mkdir(parents=True)
-            
+
+            # write the index.html
             with blogBuildPath.open('w') as blogHtmlFile:
                 blogHtmlFile.write(subedHtml)
-            
-            # move all .png/.jpg/.gif file to the same folder
 
+            # copy all .png/.jpg/.gif file to the same folder
+            for filePath in blogFolderPath.iterdir():
+                if(filePath.suffix in picFormats):
+                    targetPath = blogBuildFolderPath/filePath.name
+                    copy(str(filePath), str(targetPath))
             pass
-
+        print('Update completed!')
         pass
 
     # return 'This is just a test command line app!'
